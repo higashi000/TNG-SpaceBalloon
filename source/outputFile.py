ecddd204496c3049fad2d69ee454ac0a28503b15
higashi000 <higashi.ict@gmail.com>
2019-04-11 21:37:23 +0900

    [add] flush

--- source/outputFile.py
+++ source/outputFile.py
@@ -2,11 +2,17 @@ class OutputFile:
     def __init__(self, name = ''):
         self.name = name
 
-    def output_file(self, time, sensor_value):
-        self.time = time
+
+    def output_file(self, _time, sensor_value):
+        self.time = _time
         self.sensor_value = sensor_value
 
         file_name = self.name + str(self.time) + '.txt'
 
-        with open(file_name, 'w') as file:
-            file.write(str(self.sensor_value))
+        with open(file_name, 'w') as f:
+            f.write(str(self.sensor_value))
+            f.flush()
+
+        with open(file_name, 'r') as f:
+            file_str = f.read()
+            print(file_str)

a0cc4fb6f12af687cab95e3a9e40e690f0a8e2ac
higashi000 <higashi.ict@gmail.com>
2019-04-17 10:30:11 +0900

    [change] start script

--- source/start.sh
+++ source/start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-python3 ./temperatureSensor.py $1
+_i=1
+_one=1
 
-sync
+while :
+do
+  _nowTime=`date "+%H%m%S"`
+  _canGetTemperature=$(($_i%60))
+  if [ $_canGetTemperature = 0 ]; then
+    python3 ./temperatureSensor.py $_nowTime &
+  fi
+
+  python3 ./gyroSensor.py $_nowTime &
+
+  _i=$(($_i + $_one))
+  sync
+  sleep 1
+done

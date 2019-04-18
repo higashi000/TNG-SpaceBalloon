5a811b6e92d39b7a44c0d9f709fabc6647a7ab8a
itok01 <itok01tech@gmail.com>
2019-04-18 17:17:03 +0900

    [更新] 新しい温度・湿度センサを使えるようにしました

--- source/start.sh
+++ source/start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-python3 ./temperatureSensor.py $1
+_i=1
+_one=1
 
-sync
+while :
+do
+  echo aaaaaaaaaaaaaaaaaaaaaaaaaa
+  _nowTime=`date "+%H%m%S"`
+  _canGetTemperature=$(($_i%60))
+  if [ $_canGetTemperature = 0 ]; then
+    echo gregreuophfgesioh@gioh@ugreuophgreauopbagrewio
+    python3 ./temperature-humidity_sensor.py $_nowTime &
+  fi
+
+  _i=$(($_i + $_one))
+  sync
+  sleep 1
+done
--- source/temperature-humidity_sensor.py
+++ source/temperature-humidity_sensor.py
@@ -1,5 +1,8 @@
 import time
 import smbus
+import sys
+import outputFile
+from time import sleep
 
 def tempChanger(msb, lsb):
     mlsb = ((msb << 8) | lsb)                                             # P1
@@ -9,16 +12,35 @@ def humidChanger(msb, lsb):
     mlsb = ((msb << 8) | lsb)
     return (100 * int(str(mlsb), 10) / (pow(2, 16) - 1))
 
-i2c = smbus.SMBus(1)
-i2c_addr = 0x45                                                           # P3
 
-i2c.write_byte_data(i2c_addr, 0x21, 0x30)                                 # P4
-time.sleep(0.5)
+def getTemp():
+    i2c = smbus.SMBus(1)
+    i2c_addr = 0x45
+    i2c.write_byte_data(i2c_addr, 0x21, 0x30)
+    sleep(0.5)
+    i2c.write_byte_data(i2c_addr, 0xE0, 0x00)
+    data = i2c.read_i2c_block_data(i2c_addr, 0x00, 6)
+    return str('{:.4g}'.format(tempChanger(data[0], data[1])))
 
-while 1:
-    i2c.write_byte_data(i2c_addr, 0xE0, 0x00)                             # P5
-    data = i2c.read_i2c_block_data(i2c_addr, 0x00, 6)                     # P6
-    print( str('{:.4g}'.format(tempChanger(data[0], data[1]))) + "C" )
-    print( str('{:.4g}'.format(humidChanger(data[3], data[4]))) + "%" )
-    print("------")
-    time.sleep(1)
+def getHumid():
+    i2c = smbus.SMBus(1)
+    i2c_addr = 0x45
+    i2c.write_byte_data(i2c_addr, 0x21, 0x30)
+    sleep(0.5)
+    i2c.write_byte_data(i2c_addr, 0xE0, 0x00)
+    data = i2c.read_i2c_block_data(i2c_addr, 0x00, 6)
+    return str('{:.4g}'.format(humidChanger(data[3], data[4])))
+
+args = sys.argv
+
+try:
+    tempOutput = outputFile.OutputFile('temperature')
+    outputTempValue = getTemp()
+    tempOutput.output_file(args[1], outputTempValue)
+    humidOutput = outputFile.OutputFile('humidity')
+    outputHumidValue = getHumid()
+    humidOutput.output_file(args[1], outputHumidValue)
+    sleep(1)
+
+except KeyboardInterrupt:
+    pass 

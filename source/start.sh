7d77237c303c3eedffc3b444c9400ca50813b369
higashi <higashi.ict@gmail.com>
2019-04-19 22:26:31 +0900

    Update start.sh

--- source/start.sh
+++ source/start.sh
@@ -5,13 +5,13 @@ _one=1
 
 while :
 do
-  echo aaaaaaaaaaaaaaaaaaaaaaaaaa
   _nowTime=`date "+%H%m%S"`
   _canGetTemperature=$(($_i%60))
   if [ $_canGetTemperature = 0 ]; then
-    echo gregreuophfgesioh@gioh@ugreuophgreauopbagrewio
-    python3 ./temperature-humidity_sensor.py $_nowTime &
+    python3 ./temperatureSensor.py $_nowTime &
   fi
+
+  python3 ./gyroSensor.py $_nowTime &
 
   _i=$(($_i + $_one))
   sync

ecd10ba20f1e6e17ff81a55c9f3bddd37f1bfc53
ebina0123 <ebina1105@gmail.com>
2019-04-22 22:50:02 +0900

    [add] ファイルへの書き込み

--- source/temperature_typeK.py
+++ source/temperature_typeK.py
@@ -2,14 +2,18 @@
 from max31855 import MAX31855, MAX31855Error
 import RPi.GPIO as GPIO
 import time 
+import outputFile
+import sys
 
 cs_pin = 24
 clock_pin = 23
 data_pin = 21
 units = "c"
 
-while 1:
-	thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units, GPIO.BOARD)
-	print(thermocouple.get())
-	thermocouple.cleanup()
-	time.sleep(1)
+args = sys.argv
+
+output = outputFile.OutputFile('temperature_K_')
+
+thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units, GPIO.BOARD)
+output.output_file(args[1], thermocouple.get())
+thermocouple.cleanup()

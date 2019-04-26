99fe0d1926f8d94e112ef5167665355b761ef2fc
higashi000 <higashi.ict@gmail.com>
2019-04-26 21:27:50 +0900

    [add] 取得した値をファイルに書き出しするようにした

--- source/temperature_typeK.py
+++ source/temperature_typeK.py
@@ -2,14 +2,24 @@
 from max31855 import MAX31855, MAX31855Error
 import RPi.GPIO as GPIO
 import time 
+import outputFile
+import sys
 
 cs_pin = 24
 clock_pin = 23
 data_pin = 21
 units = "c"
 
-while 1:
-	thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units, GPIO.BOARD)
-	print(thermocouple.get())
-	thermocouple.cleanup()
-	time.sleep(1)
+args = sys.argv
+
+now_time = args[1]
+
+hand_over_time = now_time.replace('/', '')
+
+outputTemperature_typeK = outputFile.OutputFile('temperature_K_')
+
+thermocouple = MAX31855(cs_pin, clock_pin, data_pin, units, GPIO.BOARD)
+
+outputTemperature_typeK.output_file(hand_over_time, thermocouple.get())
+
+thermocouple.cleanup()

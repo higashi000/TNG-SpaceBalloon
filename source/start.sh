17123498ccd7363484041123ae244e754c79ff4d
higashi000 <higashi.ict@gmail.com>
2019-04-27 22:35:21 +0900

    [add] 全部のセンサを統合

--- source/start.sh
+++ source/start.sh
@@ -5,13 +5,26 @@ _one=1
 
 while :
 do
-   _canGetTemperature=$(($_i%60))
-   _nowTime=`date "+%H%m%S"`
-#  if [ $_canGetTemperature = 0 ]; then
+  _canGetTemperature=$(($_i%60))
+  _canGPS=$(($_i%10))
+  _nowTime=`date "+%H%M%S"`
+  echo $_nowTime
+  if [ $_canGetTemperature = 0 ]; then
+    echo "temperature_typeK"
     python3 ./temperature_typeK.py $_nowTime &
-#  fi
+    echo "temperature humidity"
+    python3 ./temperature-humidity_sensor.py $_nowTime &
+  fi
 
-#  python3 ./acceleration_gyro_magAddress.py $_nowTime &
+  if [ $_canGPS = 0 ]; then
+    echo "gps"
+    python3 ./gps.py $_nowTime &
+  fi
+
+  echo "9_1"
+  python3 ./acceleration_gyro_magAddress.py $_nowTime &
+  echo "9_2"
+  python3 ./acceleration_gyro_magAddress2.py $_nowTime &
 
   _i=$(($_i + $_one))
   sync

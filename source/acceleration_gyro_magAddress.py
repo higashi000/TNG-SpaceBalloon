668699d9ba26293da4be043b8f44b80effc176a3
higashi000 <higashi.ict@gmail.com>
2019-04-29 23:31:50 +0900

    [remove] 磁気は取らないようにした

--- source/acceleration_gyro_magAddress.py
+++ source/acceleration_gyro_magAddress.py
@@ -56,35 +56,8 @@ def gyro() :
 
 # }}}
 
-# Mag address {{{
-
-def magAddress() :
-    bus.write_byte_data(0x13, 0x4C, 0x00)
-    bus.write_byte_data(0x13, 0x4E, 0x84)
-    bus.write_byte_data(0x13, 0x51, 0x04)
-    bus.write_byte_data(0x13, 0x52, 0x0F)
-
-#   time.sleep(0.5)
-
-    data = bus.read_i2c_block_data(0x13, 0x42, 6)
-
-    xMag = ((data[1] * 256) + (data[0] & 0xF8)) / 8
-    if xMag > 4095 :
-        xMag -= 8192
-    yMag = ((data[3] * 256) + (data[2] & 0xF8)) / 8
-    if yMag > 4095 :
-        yMag -= 8192
-    zMag = ((data[5] * 256) + (data[4] & 0xFE)) / 2
-    if zMag > 16383 :
-        zMag -= 32768
-
-    return str(xMag) + ',' + str(yMag) + ',' + str(zMag) + '\n'
-
-# }}}
-
 outputAccleration = outputFile.OutputFile('acceleration')
 outputGyro = outputFile.OutputFile('gyro')
-outputMagAddress = outputFile.OutputFile('MagAddress')
 
 args = sys.argv
 
@@ -95,15 +68,11 @@ hand_over_time = now_time.replace('/', '')
 i = 0.0
 acceleration_value = ""
 gyro_value = ""
-mag_address_value = ""
 
 while i <= 1 :
     acceleration_value += acceleration()
     gyro_value += gyro()
-    mag_address_value += magAddress()
-
     i += (1 / 60)
 
 outputAccleration.output_file(hand_over_time, acceleration_value)
 outputGyro.output_file(hand_over_time, gyro_value)
-outputMagAddress.output_file(hand_over_time, mag_address_value)
